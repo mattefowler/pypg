ae35737901c3a3c50aecb8c5c79e78e75bd6f1e1
Matt Fowler <mattf@nautilus.bio>
2023-06-21 13:51:34 -0700

    default to inclusive bounds for allowed range

--- tests/test_allowed_range.py
+++ tests/test_allowed_range.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from operator import ge, le
+from operator import ge, le, lt, gt
 
 from pypg import PreSet, Property, PropertyClass
 from pypg.traits.allowed_range import AllowedRange
@@ -13,24 +13,24 @@ class TestAllowedRange(TestCase):
 
         b = Bounded()
         b.p = 0.5  # within range
-        with self.assertRaises(ValueError):
-            b.p = 0  # boundaries are exlcusive by default
-        with self.assertRaises(ValueError):
-            b.p = 1  # boundaries are exlcusive by default
+        b.p = 0  # boundaries are inclusive by default
+        b.p = 1  # boundaries are inclusive by default
         with self.assertRaises(ValueError):
             b.p = -1  # below minimum
         with self.assertRaises(ValueError):
             b.p = 2  # above maximum
 
-    def test_inclusive_bounds(self):
+    def test_exclusive_bounds(self):
         class Bounded(PropertyClass):
-            ar = AllowedRange[PreSet](0, 1, min_cmp=ge, max_cmp=le)
-            p = Property[float](0, traits=[ar])
+            ar = AllowedRange[PreSet](0, 1, min_cmp=gt, max_cmp=lt)
+            p = Property[float](0.5, traits=[ar])
 
         b = Bounded()
         b.p = 0.5  # within range
-        b.p = 0  # bounds have been explicitly declared inclusive
-        b.p = 1  # bounds have been explicitly declared inclusive
+        with self.assertRaises(ValueError):
+            b.p = 0  # bounds have been explicitly declared exclusive
+        with self.assertRaises(ValueError):
+            b.p = 1  # bounds have been explicitly declared exclusive
         with self.assertRaises(ValueError):
             b.p = -1  # below minimum
         with self.assertRaises(ValueError):
