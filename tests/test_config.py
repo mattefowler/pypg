0a6e11bdb3c4ac0a635c0a0647ef081d70e40ef7
mattfnautilus <127139935+mattfnautilus@users.noreply.github.com>
2023-06-27 19:20:47 -0700

    config to_file and to_string convenience methods (#37)

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,11 @@
-from unittest import TestCase
+import json
+import os
+import tempfile
+from unittest import TestCase, mock
 
 from pypg import PropertyClass, MethodReference, Property, decode
 from pypg.traits.config import Config
+from pypg.transcode import from_file
 
 
 class InnerCls(PropertyClass):
@@ -29,3 +33,17 @@ class ConfigTest(TestCase):
 
         self.assertEqual(0, copy.excluded)
         self.assertIsNone(copy.excluded_inner)
+
+    def test_to_file(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            cdc = CfgDataCls(included=1, excluded=1, excluded_inner=InnerCls(p1=1))
+            cdc.inner.p1 = 1
+            fpath = os.path.join(tmpdir,'encoded.json')
+            Config.to_file(cdc, fpath)
+            copy = from_file(fpath)
+            self.assertEqual(cdc.inner.p1, copy.inner.p1)
+
+    def test_to_string(self):
+        cdc = CfgDataCls(included=1, excluded=1, excluded_inner=InnerCls(p1=1))
+        json_str = json.dumps(Config.encode(cdc))
+        self.assertEqual(json_str, Config.to_string(cdc))
