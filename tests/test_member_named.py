d78e601480f8f2beaa102cbc789dc62e526430b4
Matt Fowler <mattefowler@gmail.com>
2023-12-27 11:00:22 -0800

    add array element naming

--- tests/test_member_named.py
+++ tests/test_member_named.py
@@ -2,10 +2,20 @@ from unittest import TestCase
 
 from pypg import Property, PropertyClass
 from pypg.named import Named
+from pypg.traits.member_named import MemberNameElements
 
 
 class Composer(PropertyClass):
     inner = Property[Named](lambda _: Named())
+
+
+class ArrayComposer(PropertyClass):
+    def _get_composers(self):
+        return [Composer() for i in range(2)]
+
+    composers = Property[list[Composer]](
+        _get_composers, traits=[MemberNameElements()]
+    )
 
 
 class TestMemberNamed(TestCase):
@@ -14,3 +24,9 @@ class TestMemberNamed(TestCase):
         self.assertEqual(composer.inner.name, "inner")
         composer = Composer(inner=Named(name="test"))
         self.assertEqual(composer.inner.name, "test")
+
+    def test_name_array(self):
+        ac = ArrayComposer()
+        for i, c in enumerate(ac.composers):
+            self.assertEqual(c.name, f"composers {i}")
+            self.assertEqual(c.inner.name, "inner")
